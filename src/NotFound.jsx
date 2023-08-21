54140e7896196d85f3c43927a35f7c53e752f1ac
Eyouel Haile <eyuataait@gmail.com>
2023-08-21 17:48:32 +0300

    final commit

---
+++ src/NotFound.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+const NotFound = () => {
+    return ( 
+        <div className="not-found">
+            <h2>Page not found</h2>
+            <Link to="/"> Home </Link>
+        </div>
+        
+     );
+}
+ 
+export default NotFound;
